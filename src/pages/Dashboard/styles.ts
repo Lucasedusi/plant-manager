4870efff04f8ce1398f88d7dda91c1f0bbc79592
Lucasedusi <lucasmto.16@hotmail.com>
2021-05-04 10:01:31 -0300

    create services folders

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -9,6 +9,12 @@ export const Content = styled.View`
   padding: 10px 30px;
 `;
 
+export const List = styled.FlatList`
+  height: 48px;
+  padding-bottom: 5px;
+  margin: 20px 0 0 32px;
+`;
+
 export const Title = styled.Text`
   font-size: 17px;
   color: #52665a;
